5f56f344adf08db6311e071a58a64ff884f59672
Van <vanhornegamer@gmail.com>
2026-04-07 18:43:21 -0700

    Update fnah.py
    keys 0-10 as well as - and = change background (placeholder backgrounds added in previous commit)

--- fnah.py
+++ fnah.py
@@ -19,6 +19,34 @@ while running:
         if event.type == pygame.QUIT: # hkey dont know how it works but it ends the game
             running = False
 
+    keyis = pygame.key.get_pressed() # defines keyis (key is pressed)
+    # placeholder test
+    # press keys to switch 'rooms'
+    if keyis[pygame.K_0]:
+        room = pygame.image.load('Resources/room0.png')
+    if keyis[pygame.K_1]:
+        room = pygame.image.load('Resources/room1.png')
+    if keyis[pygame.K_2]:
+        room = pygame.image.load('Resources/room2.png')
+    if keyis[pygame.K_3]:
+        room = pygame.image.load('Resources/room3.png')
+    if keyis[pygame.K_4]:
+        room = pygame.image.load('Resources/room4.png')
+    if keyis[pygame.K_5]:
+        room = pygame.image.load('Resources/room5.png')
+    if keyis[pygame.K_6]:
+        room = pygame.image.load('Resources/room6.png')
+    if keyis[pygame.K_7]:
+        room = pygame.image.load('Resources/room7.png')
+    if keyis[pygame.K_8]:
+        room = pygame.image.load('Resources/room8.png')
+    if keyis[pygame.K_9]:
+        room = pygame.image.load('Resources/room9.png')
+    if keyis[pygame.K_MINUS]:
+        room = pygame.image.load('Resources/room10.png')
+    if keyis[pygame.K_EQUALS]:
+        room = pygame.image.load('Resources/room11.png')
+
     screen.blit(room0, (0,0)) # currently this image is 1280x720, same as the proportions i put in screen size
 
     pygame.display.flip()
