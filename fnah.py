dada4840735cf872f8b82b7d0c9dcc8a69a48d1e
vhgjthevan <vanhornegamer@gmail.com>
2026-04-10 10:31:31 -0700

    modified:   ../fnah.py 	../.DS_Store 	.DS_Store 	photos/

--- fnah.py
+++ fnah.py
@@ -14,7 +14,7 @@ pygame.display.set_caption('Five Nights at Hamber')
 camview = False
 
 # images (idk a better way to do this)
-room = pygame.image.load('Resources/room0.png')
+room = pygame.image.load('Resources/placeholders/room0.png')
 
 # while loop for the game
 # all this happens when running is TRUE
@@ -22,45 +22,48 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # hkey dont know how it works but it ends the game
             running = False
-        if event.type == pygame.KEYUP()[K_SPACE]:
-            if camview:
-                camview = False
-            else:
-                camview = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                if camview:
+                    camview = False
+                else:
+                    camview = True
 
     keyis = pygame.key.get_pressed() # defines keyis (key is pressed) (can be anything thats just the name i gave to the variable)
     # placeholder image test
     # press keys to switch 'rooms'
     if keyis[pygame.K_0]:
-        room = pygame.image.load('Resources/room0.png')
+        room = pygame.image.load('Resources/photos/room0.png')
     if keyis[pygame.K_1]:
-        room = pygame.image.load('Resources/room1.png')
+        room = pygame.image.load('Resources/photos/room1.png')
     if keyis[pygame.K_2]:
-        room = pygame.image.load('Resources/room2.png')
+        room = pygame.image.load('Resources/photos/room2.png')
     if keyis[pygame.K_3]:
-        room = pygame.image.load('Resources/room3.png')
+        room = pygame.image.load('Resources/photos/room3.png')
     if keyis[pygame.K_4]:
-        room = pygame.image.load('Resources/room4.png')
+        room = pygame.image.load('Resources/photos/room4.png')
     if keyis[pygame.K_5]:
-        room = pygame.image.load('Resources/room5.png')
+        room = pygame.image.load('Resources/photos/room5.png')
     if keyis[pygame.K_6]:
-        room = pygame.image.load('Resources/room6.png')
+        room = pygame.image.load('Resources/photos/room6.png')
     if keyis[pygame.K_7]:
-        room = pygame.image.load('Resources/room7.png')
+        room = pygame.image.load('Resources/photos/room7.png')
     if keyis[pygame.K_8]:
-        room = pygame.image.load('Resources/room8.png')
+        room = pygame.image.load('Resources/photos/room8.png')
     if keyis[pygame.K_9]:
-        room = pygame.image.load('Resources/room9.png')
+        room = pygame.image.load('Resources/photos/room9.png')
     if keyis[pygame.K_MINUS]:
-        room = pygame.image.load('Resources/room10.png')
+        room = pygame.image.load('Resources/photos/room10.png')
     if keyis[pygame.K_EQUALS]:
-        room = pygame.image.load('Resources/room11.png')
+        room = pygame.image.load('Resources/photos/room11.png')
 
     screen.blit(room, (0,0)) # currently this image is 1280x720, same as the proportions i put in screen size
 
     # camera window - wip
     if camview:
-        pygame.draw.rect(screen, (89, 78, 65), (room.get_width()-10, room.get_height()-10, room.get_width()+20, room.get_height()+20))
+        # pygame.draw.rect(screen, (89, 78, 65), (room.get_width()-10, room.get_height()-10, room.get_width()+20, room.get_height()+20))
+        screen.blit(room, (500, 200))
+        pygame.transform.smoothscale(room, (500, 375))
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000 # this does something i think
