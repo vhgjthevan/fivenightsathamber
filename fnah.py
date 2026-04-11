6c8da2ab2b01556a594f906675d653a029598666
vhgjthevan <vanhornegamer@gmail.com>
2026-04-11 09:08:04 -0700

    new file:   Resources/placeholders/securityofficebottom.png 	new file:   Resources/placeholders/securityofficemiddle.png 	new file:   Resources/placeholders/securityofficetop.png 	modified:   fnah.py
    Few big changes!
    
    I finally replaced the background placeholder with something new, probably also a placeholder for now but it has three layers, so we can put sprites in between them to look cool.
    
    I GOT THE IMAGE PANNING TO WORK!!!! This def took the longest by far, since its a lot of numbers I guess, sort of confusing. I did my best to comment the code so hopefully yall understand whatever I was on at 10:30pm last night.
    
    Camera can now be toggled with mouse click! I still need to make the button only functional when the screen is panned all the way to the right.
    
    Pressing ESC quits the game.
    
    >[^D]
    ^ tenna deltarune

--- fnah.py
+++ fnah.py
@@ -11,29 +11,40 @@ screen = pygame.display.set_mode((1280, 720))
 scheight = screen.get_height()
 scwidth = screen.get_width()
 clock = pygame.time.Clock()
+
+# misc
 running = True
 pygame.display.set_caption('Five Nights at Hamber')
 camview = False
+panspeed = 500
 
 # images (idk a better way to do this)
-playerroom = pygame.image.load('Resources/placeholders/room0.png')
 room = pygame.transform.smoothscale(pygame.image.load('Resources/photos/hambcaf.JPG'), (500, 375))
+roombottom = pygame.image.load('Resources/placeholders/securityofficebottom.png')
+roommiddle = pygame.image.load('Resources/placeholders/securityofficemiddle.png')
+roomtop = pygame.image.load('Resources/placeholders/securityofficetop.png')
+camdown = pygame.image.load('Resources/placeholders/cameradown.png')
+camup = pygame.image.load('Resources/placeholders/cameraup.png')
+
+margin = (roomtop.get_width() - scwidth) / 2
+imagepan = 0
 
 # while loop for the game
 # all this happens when running is TRUE
 while running:
+    mouseis = pygame.mouse.get_pressed()[0]
+    camstatus = False
+    dt = clock.tick(60) / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # hkey dont know how it works but it ends the game
             running = False
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mx, my = pygame.mouse.get_pos()
+            if mx in range(scwidth-75, scwidth) and my in range(75, 75+250):
                 if camview:
                     camview = False
                 else:
                     camview = True
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_ESCAPE:
-                pygame.display.set_mode(pygame.FULLSCREEN)
 
     keyis = pygame.key.get_pressed() # defines keyis (key is pressed) (can be anything thats just the name i gave to the variable)
     # placeholder image test
@@ -63,17 +74,37 @@ while running:
     if keyis[pygame.K_EQUALS]:
         room = pygame.image.load('Resources/placeholders/room11.png')
     if keyis[pygame.K_ESCAPE]:
-        running = False
+        running = False # esc to quit
 
-    screen.blit(playerroom, (0,0)) # currently this image is 1280x720, same as the proportions i put in screen size
+    screen.blit(roombottom, (imagepan, 0)) # room has three layers, so we can place enemy sprites in between bottom and middle
+    screen.blit(roommiddle, (imagepan, 0))
+    screen.blit(roomtop, (imagepan, 0))
+    camhb = pygame.Rect(scwidth-75, 75, camup.get_width(), camup.get_height()) # camera button hitbox
 
-    # camera window - wip
+    # uses transform.smoothscale to anti-alias the image when it scales it down (makes it look less pixelated)
     if camview:
-        screen.blit(pygame.transform.smoothscale(room, (700, 525)), (scwidth-700, 0))
+        screen.blit(pygame.transform.smoothscale(room, (700, 525)), (scwidth-700-75, 0)) # first tuple contains the calculated w/h of the scaled image, second tuple subracts image width and camera icon width from screen width for the x, and 0 for the y
+        screen.blit(camup, (scwidth-75, 75))
     else:
-        screen.blit(pygame.image.load('Resources/placeholders/cameraicon.png'), (scwidth-75, 75))
+        screen.blit(camdown, (scwidth-75, 75))
+
+    if imagepan <= -399:
+        camstatus = True
+    else:
+        camstatus = False
+
+    mx, my = pygame.mouse.get_pos()
+    if camview == False:
+        if mx < 150:
+            imagepan += panspeed * dt
+        if mx > (scwidth-150):
+            imagepan -= panspeed * dt
+        if imagepan < -margin * 2:
+            imagepan = -margin * 2
+        if imagepan > 0:
+            imagepan = 0
 
 
     pygame.display.flip()
-    dt = clock.tick(60) / 1000 # this does something i think
 
+pygame.quit()
