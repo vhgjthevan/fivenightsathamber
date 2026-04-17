9e011d306413b5fc48647c165fa8e67df32ff7b6
vhgjthevan <vanhornegamer@gmail.com>
2026-04-17 09:57:23 -0700

    new file:   fnah_new.py
    we're so fried gang we need to fix ts
    
    >[^D] < tenna deltarune

---
+++ fnah_new.py
@@ -0,0 +1,104 @@
+import pygame
+def camchangechecker(camview, camviewhelper):
+    if camviewhelper != camview:
+        camviewhelper = camview
+        print(f"camview changed to {camview}")
+        return True
+    else:
+        return False
+
+
+
+pygame.init()
+pygame.display.set_icon(pygame.image.load('Resources/placeholders/fnah_logo.png'))
+screen = pygame.display.set_mode((1280, 720))
+scheight = screen.get_height()
+scwidth = screen.get_width()
+clock = pygame.time.Clock()
+mouseis = pygame.mouse.get_pressed()[0]
+mode = 'game'
+
+running = True
+pygame.display.set_caption('Five Nights at Hamber')
+camview = False
+camviewhelper = False
+panspeed = 500
+
+camroom = pygame.transform.smoothscale(pygame.image.load('Resources/photos/hambcaf.JPG'), (500, 375))
+camroombottom = pygame.image.load('Resources/placeholders/securityofficebottom.png')
+camroommiddle = pygame.image.load('Resources/placeholders/securityofficemiddle.png')
+camroomtop = pygame.image.load('Resources/placeholders/securityofficetop.png')
+camdown = pygame.image.load('Resources/placeholders/cameradown.png')
+camup = pygame.image.load('Resources/placeholders/cameraup.png')
+
+margin = (camroomtop.get_width() - scwidth) / 2
+imagepan = 0
+
+while running:
+    dt = clock.tick(60) / 1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if mode == 'game':
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mx, my = pygame.mouse.get_pos()
+                if mx in range(scwidth-75, scwidth) and my in range(75, 75+250):
+                    if camview:
+                        print("changed camview to False")
+                        camview = False
+                    else:
+                        print("changed camview to True")
+                        camview = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_0:
+                room = pygame.image.load('Resources/photos/hambcaf.JPG')
+            if event.key == pygame.K_1:
+                room = pygame.image.load('Resources/photos/hambclass.JPG')
+            if event.key == pygame.K_2:
+                room = pygame.image.load('Resources/photos/hambhallway.JPG')
+            if event.key == pygame.K_3:
+                room = pygame.image.load('Resources/photos/hambgym.JPG')
+            if event.key == pygame.K_4:
+                room = pygame.image.load('Resources/photos/hambhuddle.JPG')
+            if event.key == pygame.K_5:
+                room = pygame.image.load('Resources/photos/hamblibrary.JPG')
+            if event.key == pygame.K_6:
+                room = pygame.image.load('Resources/photos/hambmath.JPG')
+            if event.key == pygame.K_7:
+                room = pygame.image.load('Resources/photos/hambsci.JPG')
+            if event.key == pygame.K_8:
+                room = pygame.image.load('Resources/photos/hambsideent.JPG')
+            if event.key == pygame.K_9:
+                room = pygame.image.load('Resources/photos/hambstairs.JPG')
+            if event.key == pygame.K_MINUS:
+                room = pygame.image.load('Resources/placeholders/room10.png')
+            if event.key == pygame.K_EQUALS:
+                room = pygame.image.load('Resources/placeholders/room11.png')
+            if event.key == pygame.K_ESCAPE:
+                running = False # esc to quit
+
+
+    screen.blit(camroombottom, (imagepan, 0)) # room has three layers, enemies will be placed on the middle layer.
+    screen.blit(camroommiddle, (imagepan, 0))
+    screen.blit(camroomtop, (imagepan, 0))
+    #check if the camview var has changed
+    if camchangechecker(camview, camviewhelper):
+        if camview:
+            screen.blit(pygame.transform.smoothscale(camroom, (700, 525)), (scwidth-700-75, 0)) #first tuple contains the w/h of the scaled img. second tuple subtracts img width and cam icon width from scwidth for the x, and 0 for the y
+            screen.blit(camup, (scwidth-75, 75))
+        else:
+            screen.blit(camdown, (scwidth-75, 75))
+    if camview == False:
+        mx, my = pygame.mouse.get_pos()
+        if mx<150:
+            imagepan += panspeed*dt
+        elif mx>(scwidth-150):
+            imagepan -= panspeed*dt
+        if imagepan < -margin*2:
+            imagepan = -margin*2
+        if imagepan > 0:
+            imagepan = 0
+
+    pygame.display.flip()
+
+pygame.quit()
